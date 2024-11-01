86c3ed9e6939721948708d627fcb6f700e307c05
Viransh Shah <shahv47@mcmaster.ca>
2024-10-31 22:37:50 -0400

    test

--- src-tauri/main.js
+++ src-tauri/main.js
@@ -18,7 +18,7 @@ if (!targetTriple) {
 }
 
 if (targetTriple === "aarch64-apple-darwin") {
-  targetTriple = os.arch() === "arm64" ? targetTriple : "x86_64-apple-darwin";
+  targetTriple = os.arch() === "x64" ? targetTriple : "x86_64-apple-darwin";
 }
 
 // Rename the binary using the target triple
