41886ea5c3cbe61372fd20dd45e0a6edccc10a33
Viransh Shah <shahv47@mcmaster.ca>
2025-01-15 03:51:54 -0500

    update actions

--- backend/config.py
+++ backend/config.py
@@ -9,15 +9,16 @@ class Config:
         else os.path.dirname(os.path.realpath(__file__))
     )
     conda_prefix = os.environ.get("CONDA_PREFIX")
+    lib = "Library" if sys.platform == "win32" else ""
     PROJ_LIB = (
-        os.path.join(sys._MEIPASS, "Library/share/proj")
+        os.path.join(sys._MEIPASS, lib, "share/proj")
         if getattr(sys, "frozen", False)
-        else os.path.join(conda_prefix, "Library/share/proj")
+        else os.path.join(conda_prefix, lib, "share/proj")
     )
     GDAL_DATA = (
-        os.path.join(sys._MEIPASS, "Library/share/gdal")
+        os.path.join(sys._MEIPASS, lib, "share/gdal")
         if getattr(sys, "frozen", False)
-        else os.path.join(conda_prefix, "Library/share/gdal")
+        else os.path.join(conda_prefix, lib, "share/gdal")
     )
     EXPORT_PATH = os.getenv("EXPORT_PATH", "dataExport")
     LOOKUP = "Jenette_Creek_Watershed/Database/lookup.db3"
