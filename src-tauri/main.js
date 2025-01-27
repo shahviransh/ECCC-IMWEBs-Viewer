ef8a693e0b95f9c5123009db219467abd7671694
Viransh Shah <shahv47@mcmaster.ca>
2025-01-27 18:26:06 -0500

    release:app

--- src-tauri/main.js
+++ src-tauri/main.js
@@ -7,7 +7,6 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 const ext = process.platform === "win32" ? ".exe" : "";
 
 const rustInfo = execSync("rustc -vV");
@@ -18,7 +17,13 @@ if (!targetTriple) {
 }
 
 // Rename the binary using the target triple
-const oldPath = path.resolve(__dirname, "..", "backend", "apppy", `apppy${ext}`);
+const oldPath = path.resolve(
+  __dirname,
+  "..",
+  "backend",
+  "apppy",
+  `apppy${ext}`
+);
 const newPath = path.resolve(
   __dirname,
   "..",
@@ -31,6 +36,18 @@ try {
   if (fs.existsSync(oldPath)) {
     fs.copyFileSync(oldPath, newPath);
     console.log(`Copied sidecar to ${newPath}`);
+    if (targetTriple === "aarch64-apple-darwin") {
+      fs.chmodSync(
+        oldPath,
+        path.resolve(
+          __dirname,
+          "..",
+          "backend",
+          "apppy",
+          `apppy-x86_64-apple-darwin${ext}`
+        )
+      );
+    }
   } else {
     console.error(`File ${oldPath} does not exist`);
   }
