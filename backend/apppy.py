3b249d2205a15802993ed9e674f9be0e750c1565
Viransh Shah <shahv47@mcmaster.ca>
2025-03-28 18:10:08 -0400

    release:app

--- backend/apppy.py
+++ backend/apppy.py
@@ -3,6 +3,11 @@ from flask_cors import CORS
 from flask_caching import Cache
 from routes import register_routes
 from error_handlers import register_error_handlers
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -15,4 +20,8 @@ register_routes(app, cache)
 register_error_handlers(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if os.getenv("PRODUCTION") == "true":
+        from waitress import serve
+        serve(app, host="127.0.0.1", port=5000)
+    else:
+        app.run(debug=True)
