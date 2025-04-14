8122ce37bb5be5413f35f2af3c5ab17de056c512
Viransh Shah <89473175+shahviransh@users.noreply.github.com>
2025-04-14 13:06:31 -0400

    Create MoveToIIS.ps1

---
+++ MoveToIIS.ps1
@@ -0,0 +1,8 @@
+$projectDir = "C:\ECCC-IMWEBs-Viewer"
+$sourceDir = "$projectDir\dist"
+$destDir = "C:\inetpub\wwwroot\IMWEBs-Viewer"
+
+Set-Location $projectDir
+
+# Copy the built files to the web server directory
+Copy-Item -Path $sourceDir -Destination $destDir -Recurse -Force
