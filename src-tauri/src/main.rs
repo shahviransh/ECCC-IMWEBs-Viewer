1fcf67b06c7367521e68d2cab209c061793d9d43
Viransh Shah <shahv47@mcmaster.ca>
2025-04-15 18:17:35 -0400

    release:app no need for local app data

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,50 +3,17 @@
 
 use reqwest::Client;
 use std::{ env, process::Command};
-use std::fs;
-use std::path::Path;
 use std::path::PathBuf;
 
 #[cfg(target_os = "windows")]
 const CREATE_NO_WINDOW: u32 = 0x08000000;
-
-fn get_local_appdata_path(app_name: &str) -> PathBuf {
-    #[cfg(target_os = "windows")]
-    {
-        let base = env::var("LOCALAPPDATA").expect("LOCALAPPDATA not set");
-        return PathBuf::from(base).join(app_name);
-    }
-
-    #[cfg(target_os = "linux")]
-    {
-        let base = env::var("XDG_DATA_HOME")
-            .map(PathBuf::from)
-            .unwrap_or_else(|_| {
-                let home = env::var("HOME").expect("HOME not set");
-                PathBuf::from(home).join("/usr/lib")
-            });
-        return base.join(app_name);
-    }
-
-    #[cfg(target_os = "macos")]
-    {
-        let home = env::var("HOME").expect("HOME not set");
-        return PathBuf::from(home)
-            .join("Library")
-            .join("Application Support")
-            .join(app_name);
-    }
-}
 
 #[tauri::command]
 async fn start_server() {
     // Get the current executable's directory
     let current_exe = env::current_exe().expect("Failed to get current executable path");
     let exe_dir = current_exe.parent().expect("Failed to get parent directory");
-    let up_folder = exe_dir.join("_up_");
-
-    let local_appdata = get_local_appdata_path("IMWEBs-Viewer");
-    let mut backend_dir = local_appdata.join("_up_");
+    let backend_dir = exe_dir.join("_up_");
 
     // Fetch the target triple using the environment variables or a fallback
     let target_triple = format!(
