bd4574b68d293af3b9144ba07476118c9c2cb940
Viransh Shah <89473175+shahviransh@users.noreply.github.com>
2025-04-28 19:48:21 -0400

    Update MoveToIIS.ps1

--- MoveToIIS.ps1
+++ MoveToIIS.ps1
@@ -4,15 +4,6 @@ $destDir = "C:\inetpub\wwwroot\IMWEBs-Viewer"
 
 Set-Location $projectDir
 
-$localHash = git rev-parse HEAD
-git fetch origin
-$remoteHash = git rev-parse origin/main
-
-# Check if the local and remote hashes are the same
-if ($localHash -eq $remoteHash) {
-    exit 0   
-}
-
 # Copy the built files to the web server directory
 Copy-Item -Path $sourceDir -Destination $destDir -Recurse -Force
 
