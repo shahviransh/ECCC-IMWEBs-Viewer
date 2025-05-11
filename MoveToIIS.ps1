9fd2b8e1965fbd3a8c9ae6522474afb75cde662a
Viransh Shah <89473175+shahviransh@users.noreply.github.com>
2025-05-10 23:14:35 -0400

    Update MoveToIIS.ps1

--- MoveToIIS.ps1
+++ MoveToIIS.ps1
@@ -1,5 +1,5 @@
 $projectDir = "C:\ECCC-IMWEBs-Viewer"
-$sourceDir = "$projectDir\dist\*"
+$sourceDir = "$projectDir\dist"
 $destDir = "C:\inetpub\wwwroot\IMWEBs-Viewer"
 
 Set-Location $projectDir
@@ -9,5 +9,5 @@ if (Test-Path $destDir) {
     Remove-Item -Path $destDir -Recurse -Force
 }
 
-# Copy the entire dist folder to the destination
+# Copy the entire dist folder's contents, preserving structure
 Copy-Item -Path $sourceDir -Destination $destDir -Recurse -Force
