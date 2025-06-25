d695957fddd0a8d73e7c7e8287540bf8feab344e
shahviransh <shahv47@mcmaster.ca>
2025-06-25 16:01:17 -0400

    Fix Multi table select, concat when no common columns and keep columns = 'All'

--- backend/services.py
+++ backend/services.py
@@ -109,7 +109,7 @@ def fetch_data_service(data):
                             fetch_columns.add(col)
 
                 # Remove fetched columns from columns list
-                columns = list(set(columns) - set(fetch_columns)) + [date_type] + ID
+                columns = list(set(columns) - set(fetch_columns)) + [date_type] + ID if columns != "All" else "All"
 
                 if not fetch_columns:
                     # If there are no common columns, skip the table
@@ -154,7 +154,7 @@ def fetch_data_service(data):
                             table["table"]
                         ):
                             merge_on_columns.append(col)
-                    df = pd.merge(df, df_temp, on=merge_on_columns, how="inner")
+                    df = pd.merge(df, df_temp, on=merge_on_columns, how="inner") if merge_on_columns else pd.concat([df, df_temp], axis=1)
                     # Drop rows with NaN in the required columns
                     df.dropna(inplace=True)
             except Exception as e:
