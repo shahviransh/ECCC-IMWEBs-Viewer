67e67dbd9a06b1ae8ab09379724282e0f1afc5a5
Viransh Shah <shahv47@mcmaster.ca>
2025-06-26 13:12:00 -0400

    Hide stats container based on statsColumns

--- src/components/TableStatsDisplay.vue
+++ src/components/TableStatsDisplay.vue
@@ -22,7 +22,7 @@
             </div>
         </div>
         <!-- Stats Container with Scrollable Body -->
-        <div class="stats-container">
+        <div class="stats-container" v-if="statsColumns && statsColumns.length">
             <table class="styled-table">
                 <thead>
                     <tr>
